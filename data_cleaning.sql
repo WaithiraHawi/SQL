2fab101f8d3a599f85b47dc654b09c92ffe8bf2b
Waithira Kamau <waithirak.dorothy@gmail.com>
2026-03-04 08:25:22 +0300

    data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -3,3 +3,19 @@ CREATE TABLE retail_clean AS
 SELECT *
 FROM retail_raw
 WHERE invoice_no NOT LIKE 'C%';
+
+-- drop null customers 
+DELETE FROM retail_clean
+WHERE customer_id IS NULL;
+
+-- Remove Negative or Zero Values
+DELETE FROM retail_clean
+WHERE quantity <= 0
+   OR unit_price <= 0;
+
+-- add evenue columns 
+ALTER TABLE retail_clean
+ADD COLUMN revenue NUMERIC(12,2);
+
+UPDATE retail_clean
+SET revenue = quantity * unit_price;
